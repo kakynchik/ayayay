5049bdbfb3a81cfb88464f04b30fcbaba0fe8c40
kakynchik <ukakyn@gmail.com>
2023-05-04 10:45:38 -0700

    trete zavdannya

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-class student():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def into(self):
-        print(f"Імя: {self.name}, vik: {self.age}")
-student = student("vasya", 23)
-student.info()
+class aminals:
+    pass
+class dog(aminals):
+    def sound(self):
+        print("bark")
+bobik = dog()
+bobik.sound()
